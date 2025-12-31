1bd6f66e5d843e0c4fe51e26c7e3e1ccafe41f9f
Corentin <corentinys@gmail.com>
2025-12-31 14:12:00 +0100

    Mise en place du seeder Role

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
         'email',
         'password',
+        'picture',
+        'role_id'
     ];
 
     /**
