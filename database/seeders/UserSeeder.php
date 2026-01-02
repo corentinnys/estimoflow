bdd9de6bc7d2d3fda7b114f16c11161ce3dfcb72
Corentin <corentinys@gmail.com>
2026-01-02 11:15:32 +0100

    Mise en place du modification du seeder user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,16 +10,7 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // Utilisateur admin
-       /*  User::create([
-            'firstname' => 'Admin',
-            'lastname' => 'Admin',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('password'),
-            'role_id'=>1,
-            'picture'=>'admin.jpg'
-        ]); */
-
+       
         // Utilisateurs fake
         User::factory(5)->create();
     }
