2a6a5744de8087526ff5d8b99a1f74c151819a3f
Corentin <corentinys@gmail.com>
2026-01-03 15:59:04 +0100

    Mise en place de l'authentification en jwt

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+
+
+
+        if (!$token = Auth::attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function me()
+    {
+        return response()->json(Auth::user());
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json(['message' => 'Déconnexion réussie']);
+    }
+ public function refresh()
+    {
+        return response()->json([
+            'access_token' => Auth::refresh(),
+            'token_type' => 'bearer',
+        ]);
+    }
+}
